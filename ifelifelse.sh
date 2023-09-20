7b2eb59605dd2c0d18141583e58d2cf73fe89231
Lithara Perera <litharaperera2002@gmail.com>
2023-09-20 16:43:09 +0530

    modified

---
+++ ifelifelse.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ${1,,} = lithara ]; then
+	echo "Oh, you're the boss here. Welcome!"
+elif [ ${1,,} = help ]; then
+	echo "Just enter your username, duh!"
+else
+	echo "I dont know who you are. But you're not the boss of me!"
+fi
