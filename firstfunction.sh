494e8c5f3e0449ea34c15ede0a462bcf601a95af
Lithara Perera <litharaperera2002@gmail.com>
2023-09-21 00:17:23 +0530

    modified

--- firstfunction.sh
+++ firstfunction.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-#
-#showuptime(){
-#	up=$(uptime -p | cut -c4-)
-#	since=$(uptime -s)
-#	cat << EOF
-#-----
-#This machine has been up for ${up}
-#It has been running since ${since}
-#-----
-#EOF
-#}
-#showuptime
+up="before"
+since="function"
+echo $up
+echo $since
+showuptime(){
+	local up=$(uptime -p | cut -c4-)
+	local since=$(uptime -s)
+	cat << EOF
+-----
+This machine has been up for ${up}
+It has been running since ${since}
+-----
+EOF
+}
+showuptime
+echo $up
+echo $since
